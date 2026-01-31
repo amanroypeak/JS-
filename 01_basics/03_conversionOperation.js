0eb10accc540263c88f018bae03b82bc496cd156
Aman Roy <amandesktop45@gmail.com>
2026-01-31 09:02:28 +0000

    Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,15 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//*************************Operations************************** *
+let value = 3
+let negvalue=-value
+//console.log(negvalue);
+
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
