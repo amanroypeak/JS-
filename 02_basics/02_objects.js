f3118b15eb6ea9e05dd24d3b87912a3d0f63bd4c
Aman Roy <amandesktop45@gmail.com>
2026-02-03 06:26:10 +0000

    Objects

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -25,7 +25,7 @@ const obj4 ={5:"a", 6: "b"}
 //const obj3 = {obj1, obj2}
 //const obj3 = Object.assign({},obj1, obj2, obj4)
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+//console.log(obj3);
 
 const users = [
     {
@@ -43,10 +43,22 @@ const users = [
 
 ]
 users[1].email
-console.log(tinderUser);
+//console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+//console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser));
+//console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+const course = {
+    coursename: "amna",
+    price:"000",
+    courseInstructor:"Aman"
+}
+//course.courseInstructor
+//const {courseInstructor} = course
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
