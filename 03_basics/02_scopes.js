4653b6c18b8c1668097032c9322d151b00f76142
Aman Roy <amandesktop45@gmail.com>
2026-02-04 04:50:01 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,14 +3,48 @@ let a = 300
 if (true){
     let a = 10
     const b = 20 
-    console.log("INNER:", a);
+   // console.log("INNER:", a);
     
 
 
 }
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
 
+function one(){
+    const username = "aman"
 
+    function two(){
+        const website = "youtube"
+       // console.log(username);
+        
+    }
+   // console.log(website);
+    two()
+}
+
+one()
+
+if (true){
+    const username = "aman"
+    if (username === "aman"){
+        const website = "youtube"
+       // console.log(username + website);
+        
+    }
+    //console.log(website);
+    
+}
+//console.log(username);
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+//addTwo(5) error
+const addTwo = function(num){
+    return num + 2
+}
